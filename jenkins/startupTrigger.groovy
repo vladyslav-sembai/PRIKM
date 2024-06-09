786a8aa81d1cf66ed2e640c2f85e141afc2ad9c4
vladyslav-sembai <vladyslav.sembai.mitpa.2023@lpnu.ua>
2024-06-09 19:58:36 +0000

    Update

--- jenkins/startupTrigger.groovy
+++ jenkins/startupTrigger.groovy
@@ -1,7 +1,7 @@
 import jenkins.model.*
 
 def instance = Jenkins.getInstance()
-def job = instance.getItem('Lab_5')
+def job = instance.getItem('Lab_7')
 
 if (job) {
     println("Triggering job: ${job.name}")
